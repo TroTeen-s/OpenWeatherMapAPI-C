770dbdfdbba5b1ae8ea29bfb86b35fb7292f2dd8
MTthoas <matthias.pecquery@gmail.com>
2022-03-30 16:55:01 +0200

    CallBackAPI + Verification if file is empty in Callback

--- main.c
+++ main.c
@@ -1,37 +1,60 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <json-c/json.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-size_t write_data(void *buffer)
-{
-// printf ("%s",(char *)buffer);
-}
+bool CallBackAPI();
 
 int main(){
+
+    
+    if(CallBackAPI()){
+     
+
+
+    }
+   
+
+
+
+    return 0;
+
+
+
+}
+
+bool CallBackAPI(){
 
     CURL *hnd = curl_easy_init();
     char buf[10000];
 
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(hnd, CURLOPT_URL, "https://community-open-weather-map.p.rapidapi.com/weather?q=Lyon%2Cfr&lat=0&lon=0&id=2172797&lang=null&units=imperial&mode=xml");
+    curl_easy_setopt(hnd, CURLOPT_URL, "https://community-open-weather-map.p.rapidapi.com/weather?q=lyon%2Cfr&lat=0&lon=0&id=2172797&lang=fr&units=metric&mode=JSON");
 
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers, "X-RapidAPI-Host: community-open-weather-map.p.rapidapi.com");
     headers = curl_slist_append(headers, "X-RapidAPI-Key: 1564dcd893mshf4a2056f73b2fccp13115bjsn4974baad00bd");
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
 
-     FILE * fp = fopen("test.html", "w");
+     FILE * fp = fopen("parsedData.json", "w");
 
-    curl_easy_setopt(hnd,  CURLOPT_WRITEDATA, fp); 
-	curl_easy_setopt(hnd,  CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(hnd,  CURLOPT_WRITEDATA, fp);
 
     CURLcode ret = curl_easy_perform(hnd);
 
-        printf("Extraction in JSON done in test.html\n");
+    // Lets check if file is empty
+    // SEEK_END searches the last byte of the file.
 
-    // sleep(1);
-    return 0;
+    fseek (fp, 0, SEEK_END);
+    int size = ftell(fp);
 
-
+    if (0 == size) {
+        printf("file is empty\n");
+        return false;
+    }else{
+        return true;
+    }
 
 }
 
