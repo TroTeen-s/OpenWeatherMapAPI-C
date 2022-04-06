771d5f6bc57105ab3dc45d3553ca98d09a348288
MTthoas <matthias.pecquery@gmail.com>
2022-04-07 01:08:20 +0200

    configuration file

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 #include <json-c/json.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool CallBackAPI();
-const char * Configuration();
+#define MAX 100
+
+bool CallBackAPI(const char *City, const char *Format);
+const char Configuration();
 void json_parse();
 
 
@@ -13,116 +16,90 @@ int main(){
 
 	char buffer[1024];
     json_parse();
-
-    // const char * arrayDATA = Configuration();
-    // printf("main=%s\n", arrayDATA);
-
-    // int arrayDATALENGTH = json_object_array_length(arrayDATA);
     
     FILE *fp;
-    if(CallBackAPI()){
+   
+    return 0;
+
+}
+
+void json_parse() {
+
+    Configuration();
+
+}
+
+const char Configuration(){
+
+    FILE *fp;
+    
+    struct json_object *obj, *array, *array_obj, *obj_City, *obj_format;
+    int arraylengh, i;
+    const char * stock[10];
+    static const char filename[] = "Configuration.json";
+    obj = json_object_from_file(filename);
+    array = json_object_object_get(obj, "configuration");
+
+    json_object *new_obj = json_tokener_parse(json_object_get_string(array));
+
+    obj_City = json_object_object_get(new_obj,"city");
+    obj_format = json_object_object_get(new_obj,"format");
+
+  const char *City = json_object_get_string(obj_City);
+  const char *Format = json_object_get_string(obj_format);
+
+//   printf("Name : %s\n",City);
+//   printf("Id : %s\n",Format);
+
+    if(CallBackAPI(City, Format)){
      
         struct json_object *obj, *array, *array_obj, *array_obj_name;
         int arraylen, i;
         char name[10] = {0};
         static const char filename[] = "parsedData.json";
         obj = json_object_from_file(filename);
-        array = json_object_object_get(obj, "weather");
+        array = json_object_object_get(obj, "main");
 
-        arraylen = json_object_array_length(array);
-
-        for (i = 0; i < arraylen; i++) {
-     
-        array_obj = json_object_array_get_idx(array, i);
-  
-        array_obj_name = json_object_object_get(array_obj, "main");
-    
-        printf("main=%s\n", json_object_get_string(array_obj_name));
-
-        }
     
     }
-   
-    return 0;
+
+
 
 }
 
-
-void json_parse() {
-
-    const char * arrayDATA = Configuration();
-
-    json_object * jobj = json_tokener_parse(arrayDATA);
-
-  enum json_type type;
-  json_object_object_foreach(jobj, key, val) {
-    type = json_object_get_type(val);
-    switch (type) {
-      case json_type_array: printf("type: json_type_array, ");
-                          jobj = json_object_object_get(jobj, key);
-
-                          int arraylen = json_object_array_length(jobj);
-                          printf("Array Length: %dn",arraylen);
-                          int i;
-
-                          json_object * jvalue;
-
-                          for (i=0; i< arraylen; i++){
-                            jvalue = json_object_array_get_idx(jobj, i);
-                            printf("value[%d]: %sn",i, json_object_get_string(jvalue));
-                          }
-
-                          break;
-    }
-  }
-}
-
-
-
-
-const char * Configuration(){
-
-    FILE *fp;
-    
-    struct json_object *obj, *array, *array_obj, *array_obj_name;
-     int arraylen, i;
-    static const char filename[] = "Configuration.json";
-    obj = json_object_from_file(filename);
-    array = json_object_object_get(obj, "configuration");
-
-    arraylen = json_object_array_length(array);
-
-    // printf("Configuration=%s\n", json_object_get_string(array));
-
-    for (i = 0; i < arraylen; i++) {
-     
-        array_obj = json_object_array_get_idx(array, i);
-    
-        printf("main=%s\n", json_object_get_string(array_obj));
-
-    }
-
-
-
-
-    return json_object_get_string(array);
-
-}
-
-bool CallBackAPI(){
+bool CallBackAPI(const char *City, const char *Format){
 
     CURL *hnd = curl_easy_init();
-    char buf[10000];
+    char buf[MAX];
+    char buffer[MAX];
+    char buffer_final[MAX];
+    char buffer_format[MAX];
+
+    printf("City : %s\n",City);
+    printf("Format : %s\n",Format);
+
+    const char* str1 = "https://community-open-weather-map.p.rapidapi.com/weather?q=";
+    const char* str2 = "%2Cfr&lat=0&lon=0&id=2172797&lang=fr&units=metric&mode=";
+
+    strcat(strcpy(buf, str1), City);
+    strcat(strcpy(buffer, str2), Format);
+    strcat(strcpy(buffer_final, buf), buffer);
+
+      printf("%s\n", buffer_final);
 
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(hnd, CURLOPT_URL, "https://community-open-weather-map.p.rapidapi.com/weather?q=lyon%2Cfr&lat=0&lon=0&id=2172797&lang=fr&units=metric&mode=JSON");
+    curl_easy_setopt(hnd, CURLOPT_URL, buffer_final);
 
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers, "X-RapidAPI-Host: community-open-weather-map.p.rapidapi.com");
     headers = curl_slist_append(headers, "X-RapidAPI-Key: 1564dcd893mshf4a2056f73b2fccp13115bjsn4974baad00bd");
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
 
-    FILE * fp = fopen("parsedData.json", "w");
+      const char* str3 = "Data.";
+
+    strcat(strcpy(buffer_format, str3), Format);
+
+    FILE * fp = fopen(buffer_format, "w");
 
     curl_easy_setopt(hnd,  CURLOPT_WRITEDATA, fp);
 
@@ -130,9 +107,7 @@ bool CallBackAPI(){
 
     fclose(fp);
 
-    // Lets check if file is empty
-    // SEEK_END searches the last byte of the file.
-
+    // Lets check if file is emptyconst 
     fseek (fp, 0, SEEK_END);
     int size = ftell(fp);
 
